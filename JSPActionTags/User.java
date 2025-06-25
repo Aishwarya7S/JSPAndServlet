b54f057cb2c38e308297028a369ee95acc84b4e4
Aishwarya S <aishwarya7sk@gmail.com>
2025-06-25 12:41:22 +0530

    Create User.java

---
+++ JSPActionTags/User.java
@@ -0,0 +1,29 @@
+package com.bean;
+
+public class User {
+	private String name;
+	private String email;
+	private String regno;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getRegno() {
+		return regno;
+	}
+	public void setRegno(String regno) {
+		this.regno = regno;
+	}
+}
+
