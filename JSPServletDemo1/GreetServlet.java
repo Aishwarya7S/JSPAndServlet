6af11bfb9601b604dd2e274f0e7a72ee61656fd4
Aishwarya S <aishwarya7sk@gmail.com>
2025-07-07 12:57:30 +0530

    Create GreetServlet.java

---
+++ JSPServletDemo1/GreetServlet.java
@@ -0,0 +1,30 @@
+package com.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class GreetServlet
+ */
+@WebServlet("/GreetServlet")
+public class GreetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("username");
+		
+		request.setAttribute("greet", "Hello, "+ name + "! Welcome to Servlet and you used POST method.");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("greet.jsp");
+		
+		rd.forward(request, response);
+	}
+
+}
